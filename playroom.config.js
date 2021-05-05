f0346fc7caec5fbb3e241e9fe6114a54566b4a9c
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-05-04 22:58:27 -0300

    chore: install playroom

---
+++ playroom.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  components: './src/core/index.ts',
+  frameComponent: './src/playroom/frame.ts',
+  outputPath: './dist',
+  webpackConfig: () => ({
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts|tsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cwd: path.join(__dirname, '../..'),
+              configFile: path.resolve(
+                __dirname,
+                './node_modules/playroom/.babelrc',
+              ),
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.web.js', '.mjs', '.js', '.json', '.ts', '.tsx'],
+      modules: [path.resolve(__dirname, './node_modules'), 'node_modules'],
+    },
+  }),
+  exampleCode: `
+    <h1>test</h1>
+  `,
+};
---
+++ src/playroom/frame.ts
@@ -0,0 +1,3 @@
+export default function Frame({ children }) {
+  return children;
+}
